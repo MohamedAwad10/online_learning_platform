7e54aa623f1b3b6712fc10d95aa6057c47c85c06
MohamedAwad5 <mohamedmedo331155@gmail.com>
2024-09-19 22:37:33 +0300

    comment data intializing when bean loaded

--- src/main/java/com/gdsc/OnlineLearningPlatform/config/DataIntializer.java
+++ src/main/java/com/gdsc/OnlineLearningPlatform/config/DataIntializer.java
@@ -1,23 +1,23 @@
-package com.gdsc.OnlineLearningPlatform.config;
-
-import com.gdsc.OnlineLearningPlatform.model.Role;
-import com.gdsc.OnlineLearningPlatform.repository.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DataIntializer {
-
-    @Bean
-    CommandLineRunner init(RoleRepository roleRepository){
-
-        return args -> {
-            if(roleRepository.count() == 0){
-                roleRepository.save(new Role("STUDENT"));
-                roleRepository.save(new Role("INSTRUCTOR"));
-                roleRepository.save(new Role("ADMIN"));
-            }
-        };
-    }
-}
+//package com.gdsc.OnlineLearningPlatform.config;
+//
+//import com.gdsc.OnlineLearningPlatform.model.Role;
+//import com.gdsc.OnlineLearningPlatform.repository.RoleRepository;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DataIntializer {
+//
+//    @Bean
+//    CommandLineRunner init(RoleRepository roleRepository){
+//
+//        return args -> {
+//            if(roleRepository.count() == 0){
+//                roleRepository.save(new Role("STUDENT"));
+//                roleRepository.save(new Role("INSTRUCTOR"));
+//                roleRepository.save(new Role("ADMIN"));
+//            }
+//        };
+//    }
+//}
